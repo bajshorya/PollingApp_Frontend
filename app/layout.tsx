421d8e0f852426f8ff24a66be5eaba401481a050
SHORYA BAJ <jain.shorya11@gmail.com>
2026-01-27 11:33:59 +0530

    initial setup, Ui components, shadcn, etc

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,11 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import {
+  Geist,
+  Geist_Mono,
+  Xanh_Mono,
+  Inria_Serif,
+  Noto_Sans,
+} from "next/font/google";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -10,6 +16,24 @@ const geistSans = Geist({
 const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
   subsets: ["latin"],
+});
+
+const xanhMono = Xanh_Mono({
+  variable: "--font-xanh-mono",
+  subsets: ["latin"],
+  weight: ["400"],
+});
+
+const inriaSerif = Inria_Serif({
+  variable: "--font-inria-serif",
+  subsets: ["latin"],
+  weight: ["300", "400", "700"],
+});
+
+const notoSans = Noto_Sans({
+  variable: "--font-noto-sans",
+  subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
 });
 
 export const metadata: Metadata = {
@@ -25,7 +49,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} ${xanhMono.variable} ${inriaSerif.variable} ${notoSans.variable} antialiased bg-[#175588] `}
       >
         {children}
       </body>
